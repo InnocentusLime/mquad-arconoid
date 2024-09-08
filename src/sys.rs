606eaa6c92ada39283a398ac2db63f22bcb58c8d
InnocentusLime <innocentuslime.help@gmail.com>
2024-09-08 21:07:29 +0300

    Cleaner interop with mquad

--- src/sys.rs
+++ src/sys.rs
@@ -1,22 +1,31 @@
+// uncommenting this caues a panic. we need to investigate
+// #[no_mangle]
+// pub extern "C" fn mquad_arcanoid_crate_version() -> u32 {
+//     let major = dbg!(env!("CARGO_PKG_VERSION_MAJOR").parse::<u32>().unwrap());
+//     let minor = env!("CARGO_PKG_VERSION_MINOR").parse::<u32>().unwrap();
+//     let patch = dbg!(env!("CARGO_PKG_VERSION_PATCH").parse::<u32>().unwrap());
+
+//     (major << 24) + (minor << 16) + patch
+// }
+
 #[cfg(target_family = "wasm")]
 mod platform {
     use macroquad::prelude::error;
 
     mod imports {
         extern "C" {
-            pub fn done_loading();
-            pub fn on_mobile() -> bool;
-            pub fn device_fullscreen();
-            pub fn get_orientation() -> f32;
+            pub fn app_done_loading();
+            pub fn app_is_on_mobile() -> bool;
+            pub fn app_get_orientation() -> f32;
         }
     }
 
     pub fn done_loading() {
-        unsafe { imports::done_loading() }
+        unsafe { imports::app_done_loading() }
     }
 
     pub fn on_mobile() -> bool {
-        unsafe { imports::on_mobile() }
+        unsafe { imports::app_is_on_mobile() }
     }
 
     pub fn report_fatal_error(err: anyhow::Error) {
@@ -24,7 +33,7 @@ mod platform {
     }
 
     pub fn get_orientation() -> f32 {
-        unsafe { imports::get_orientation() }
+        unsafe { imports::app_get_orientation() }
     }
 }
 
